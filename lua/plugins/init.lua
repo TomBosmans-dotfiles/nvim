2a051155a08f625a3adf5f086e655bcaaf2cbcd1
Tom Bosmans <tom.94.bosmans@gmail.com>
2021-12-14 15:56:35 +0100

    Add Telescope with fzf extension
    source: https://github.com/nvim-telescope/telescope.nvim
    and fzf: https://github.com/nvim-telescope/telescope-fzf-native.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,6 +6,16 @@ return require('packer').startup({
     use { 'wbthomason/packer.nvim', config = require_config('packer') }
     -- Theme
     use { 'arcticicestudio/nord-vim', config = require_config('nord') }
+    -- Finder
+    use {
+      'nvim-telescope/telescope.nvim',
+      config = require_config('telescope'),
+      requires = {
+        { 'nvim-lua/plenary.nvim' },
+        { 'kyazdani42/nvim-web-devicons' },
+        { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+      },
+    }
   end,
 
   config = {
