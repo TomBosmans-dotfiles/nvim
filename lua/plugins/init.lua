e28d791302d67777150798efe331bc4e71fc27df
Tom Bosmans <tom.94.bosmans@gmail.com>
2021-12-15 12:32:59 +0100

    Add cmp with luasnip
    source: https://github.com/hrsh7th/nvim-cmp
    luasnip source: https://github.com/L3MON4D3/LuaSnip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -79,5 +79,24 @@ return require('packer').startup({
         require('plugins/tree')
       end
     }
+    -- autocompletion
+    use {
+      'hrsh7th/nvim-cmp',
+      requires = {
+        { 'hrsh7th/cmp-cmdline' },
+        { 'hrsh7th/cmp-path' },
+        { 'hrsh7th/cmp-buffer' },
+        { 'hrsh7th/cmp-nvim-lsp' },
+        {
+          'L3MON4D3/LuaSnip',
+          requires = {
+            { 'saadparwaiz1/cmp_luasnip' }
+          }
+        },
+      },
+      config = function()
+        require('plugins/cmp')
+      end
+    }
   end
 })
