5bfe9cfaae73e04cd897cf2cb9e0775044da148e
Tom Bosmans <tom.94.bosmans@gmail.com>
2021-12-15 16:23:56 +0100

    Patch nord colors for diagnostics
    Nord maintainer is not responding on the following pr: https://github.com/arcticicestudio/nord-vim/pull/282.
    Currently diagnostics colors are broken in nord theme as the highlight groups changed in nevoid 0.6.0

--- lua/plugins/nord.lua
+++ lua/plugins/nord.lua
@@ -7,4 +7,13 @@ vim.g.nord_italic_comments = 1
 vim.g.nord_cursor_line_number_background = 1
 vim.g.nord_uniform_status_lines = 1
 
+vim.cmd[[ autocmd ColorScheme nord highlight DiagnosticWarn ctermfg=3 guifg=#EBCB8B ]]
+vim.cmd[[ autocmd ColorScheme nord highlight DiagnosticError ctermfg=1 guifg=#BF616A ]]
+vim.cmd[[ autocmd ColorScheme nord highlight DiagnosticInfo ctermfg=6 guifg=#88C0D0 ]]
+vim.cmd[[ autocmd ColorScheme nord highlight DiagnosticHint ctermfg=12 guifg=#5E81AC ]]
+vim.cmd[[ autocmd ColorScheme nord highlight DiagnosticUnderlineWarn ctermfg=3 guifg=#EBCB8B gui=undercurl cterm=underline ]]
+vim.cmd[[ autocmd ColorScheme nord highlight DiagnosticUnderlineError ctermfg=1 guifg=#BF616A gui=undercurl cterm=underline ]]
+vim.cmd[[ autocmd ColorScheme nord highlight DiagnosticUnderlineInfo ctermfg=6 guifg=#88C0D0 gui=undercurl cterm=underline ]]
+vim.cmd[[ autocmd ColorScheme nord highlight DiagnosticUnderlineHint ctermfg=12 guifg=#5E81AC gui=undercurl cterm=underline ]]
+
 load_theme('nord')
