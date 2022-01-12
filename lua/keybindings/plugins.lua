b7aab50781178105f0e4dc383e24782f91e76183
Tom Bosmans <tom.94.bosmans@gmail.com>
2022-01-12 16:49:03 +0100

    Add packer keybindings

---
+++ lua/keybindings/plugins.lua
@@ -0,0 +1,7 @@
+local map = require('utils').map
+
+map('n', '<leader>pS', ':PackerSync <CR>')
+map('n', '<leader>pc', ':PackerCompile <CR>')
+map('n', '<leader>pu', ':PackerUpdate <CR>')
+map('n', '<leader>pi', ':PackerInstall <CR>')
+map('n', '<leader>ps', ':PackerStatus <CR>')
