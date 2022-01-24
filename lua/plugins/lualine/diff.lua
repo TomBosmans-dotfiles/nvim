0d4ae5de0e1c416db8a795bf94691d363da4471e
Tom Bosmans <tom.94.bosmans@gmail.com>
2022-01-24 19:00:03 +0100

    change icons

--- lua/plugins/lualine/diff.lua
+++ lua/plugins/lualine/diff.lua
@@ -2,8 +2,8 @@ return {
   'diff',
   colored = true, -- Displays a colored diff status if set to true
   symbols = {
-    added = ' ',
-    modified = '~ ',
-    removed = ' '
+    added = '+',
+    modified = '~',
+    removed = '-'
   }, -- Changes the symbols used by the diff.
 }
